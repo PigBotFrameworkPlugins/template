349bb775a82187fbb1b5cdad819cafdffb5c718e
gingmzmzx <2417481092@qq.com>
2024-08-09 22:16:55 +0800

    docs: 修改注释

--- __init__.py
+++ __init__.py
@@ -19,12 +19,14 @@ meta_data = MetaData(
     """
 )
 
+
 # 在PBF启动时、插件被装载时调用
-def _enter():
+def _enter():  # 如不需要可以删除
     logger.info("Test PBF Plugin loaded")
 
+
 # 插件开放给其他插件调用的接口
-class Api:
+class Api:  # 如不需要可以删除
     @staticmethod
     def foo():  # 可以在其他插件中通过 `pbf.pluginsManager.require("this_plugin_name").foo()` 调用
         return "bar"
@@ -34,7 +36,9 @@ class Api:
 def testCommand(event: Event):
     logger.info(f"test command was called: {event}")
     # do something
-    logger.info(Msg("恭喜！\n当你收到这条消息，意味着PBFNext的全部基本功能已经测试通过，具备生产运行条件。", event=event).send())
+    logger.info(
+        Msg("恭喜！\n当你收到这条消息，意味着PBFNext的全部基本功能已经测试通过，具备生产运行条件。", event=event).send())
+    # 这是发送一条消息，Msg类是基于Client类的封装，具体使用请见文档
 
 
 @Message(name="message handler")  # 注册消息处理器，会处理所有消息
@@ -42,15 +46,18 @@ def messageHandler(event: Event):
     logger.info(f"message handler was called: {event}")
     # do something
 
+
 @Notice(name="notice handler")  # 注册通知处理器，会处理所有通知
 def noticeHandler(event: Event):
     logger.info(f"notice handler was called: {event}")
     # do something
 
+
 @Request(name="request handler")  # 注册请求处理器，会处理所有请求
 def requestHandler(event: Event):
     logger.info(f"request handler was called: {event}")
     # do something
+
 
 @Meta(name="meta handler")  # 注册元数据处理器，会处理所有元数据
 def metaHandler(event: Event):
